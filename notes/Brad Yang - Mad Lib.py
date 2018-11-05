49e356870ad98f77e78380488854f71ef70282a2
8faces <42391893+8faces@users.noreply.github.com>
2018-11-05 14:35:19 -0800

    Mad Libs 11-5-2018

--- notes/Brad Yang - Mad Lib.py
+++ notes/Brad Yang - Mad Lib.py
@@ -1,19 +1,34 @@
-adjective = input("type in an adjective")
-noun = input("type a noun")
+adjective0 = input("type adjective")
+noun0 = input("type noun")
 
-print("I saw a %s %s" % (adjective, noun))
-adjective = input("type in an adjective")
-noun = input("type a noun")
+print("Safty Rules %s %s" % (adjective0, noun0))
 
+adjective1 = input("type in an adjective")
+noun1 = input("type a noun")
 
-adjective = input("type in an adjective")
-noun = input("type a noun")
+print("1.Do not jump on %s %s" % (adjective1, noun1))
 
-adjective = input("type in an adjective")
-noun = input("type a noun")
+adjective2 = input("type in an adjective")
+noun2 = input("type a noun")
 
-adjective = input("type in an adjective")
-noun = input("type a noun")
+print("2. Do no do this %s %s" % (adjective2, noun2))
 
-adjective = input("type in an adjective")
-noun = input("type a noun")
+adjective3 = input("type in an adjective")
+noun3 = input("type a noun")
+
+print("3. Do no do this %s %s" % (adjective3, noun3))
+
+adjective4 = input("type in an adjective")
+noun4 = input("type a noun")
+
+print("4. Do no do this %s %s" % (adjective4, noun4))
+
+adjective5 = input("type in an adjective")
+noun5 = input("type a noun")
+
+print("5. Do no do this %s in the %s" % (adjective5, noun5))
+
+adjective6 = input("type in an adjective")
+noun6 = input("type a noun")
+
+print("6. Do no do this %s %s" % (adjective6, noun6))
