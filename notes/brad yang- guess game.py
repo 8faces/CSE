18496c1b018530f29773531eedd0122c9c96339b
8faces <42391893+8faces@users.noreply.github.com>
2018-11-13 14:37:18 -0800

    geuss game

--- notes/brad yang- guess game.py
+++ notes/brad yang- guess game.py
@@ -4,15 +4,14 @@ guesses = 5
 random_number = random.randint(0, 10)
 playing = True
 while guesses > 0 and playing:
-    guess = input("Guess a number from 0-10. You have five guesses")
-    if int(guesses) == int(random_number):
+    guess = input("Guess a number from 0-10")
+    if int(guess) == int(random_number):
         print("You Win!")
         playing = False
     else:
         print("Try again")
         guesses -= 1
-    if int(guesses) > int(random_number):
+    if int(guess) < int(random_number):
         print("to low")
-    if int(guesses) < int(random_number):
+    if int(guess) > int(random_number):
         print("to high")
-        
