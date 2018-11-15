b937b6029792e1d73adbf3cc9e5a2e832c7f5252
8faces <42391893+8faces@users.noreply.github.com>
2018-11-15 14:35:37 -0800

    lucky 7's

--- notes/Brad yang - Lucky 7's.py
+++ notes/Brad yang - Lucky 7's.py
@@ -3,31 +3,17 @@ active = True
 money = 15
 rounds_played = 0
 while active and money > 0:
-    rounds_played = rounds_played + 1
-    dice_2 = random.randit(1, 6)
-    dice_1 = random.randit(1, 6)
-    print(dice_1 + dice_2)
-    if dice_1 + dice_2 == 7:
-        money = money + 4
-        print("you have won $5. now you have %d " % money)
-    else:
-        money = money -1
-        print 
+        rounds_played = rounds_played + 1
+        dice_2 = random.randint(1, 6)
+        dice_1 = random.randint(1, 6)
+        print(dice_1 + dice_2)
+        if dice_1 + dice_2 == 7:
+            money = money + 4
+            print("you have won $5. now you have %d " % money)
+        else:
+            money = money - 1
+            print("you have lost $1 and you now have %d left" % money)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+if money <= 0:
+        print("you have lost after %d rounds" % rounds_played)
+active = False
