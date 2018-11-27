41599b78077a9ff78c41aaf91202f7ecc803f12f
8faces <42391893+8faces@users.noreply.github.com>
2018-11-27 14:40:17 -0800

    notes

--- notes/list notes.py
+++ notes/list notes.py
@@ -1,3 +1,5 @@
+
+"""
 # Creating a list
 fruit = ['apples', 'oranges', 'grapes',
          'mango', 'pineapple', 'strawberries', 'banana', 'pees', 'tomato', ]    # USE SQUARE BRACKETS!!!!!!!!!
@@ -17,13 +19,73 @@ print(fruit)
 # looping through lists
 for item in fruit:
     print(item)
+"""
+"""
+
+Hero1 = ['genji', 'soldier_76', 'winston', 'Reaper', 'tracer', 
+        'wreaking_ball', 'Dva', 'hanzo', 'mercy', 'junk_rat']
+
+print(" The last thing in the list is %s" % Hero1[len(Hero1)-1])
+"""
 
 
 
 
-Hero = ['Genji', 'soldier 76', 'winston', 'Reaper', 'tracer', 'wreaking ball', ]
 
-Hero[2] = 'i am already tracer'
 
-print(" The last thing in the list is %s" % Hero[len(Hero)-1)
 
+"""
+
+Hero = ['genji', 'soldier_76', 'winston', 'Reaper', 'tracer',
+        'wreaking ball', 'chicken', 'Dva', 'hanzo', 'mercy', 'junk_rat', 'widowmaker', 'maccree']
+
+
+print(Hero[2:5])
+print(Hero[3:4])
+print(Hero[10:])
+print(Hero[:5])
+
+# adding stuff to a list (part 1)
+
+Hero.append("lulu")
+Hero.append("garen")
+print(Hero)
+
+# Addding to a list (no at the end)
+Hero.insert(2, "shaco")
+print(Hero)
+
+Hero.remove("hanzo")
+Hero.remove("genji")
+print(Hero)
+
+# Removing from a list (pt 2)
+# sometimes, you don't knowow what is in the list, but you know
+# you want to get rid of somthing at a specific index
+Hero.pop(0)
+print(Hero)
+# notice that soldier_76 is no longer in the list because was is at index 0
+"""
+"""
+number_list = ['1', '2', '3', '4', '5', '6', '7', '8', ]
+
+print(number_list[1:])
+print(number_list[2:])
+print(number_list[3:])
+
+number_list.insert(4, "4")
+
+number_list.remove("3")
+print(number_list)
+"""
+
+# avoid putting parentheses when making a list
+
+
+# changing things into a list
+string1 = "turquoise"
+list1 = list(string1)
+print(list)
+
+# change back into a string (list→string)
+print("!".join(list))   
