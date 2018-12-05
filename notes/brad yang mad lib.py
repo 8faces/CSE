7ff616a522852fd1e22999c5210c7a2579e57663
8faces <42391893+8faces@users.noreply.github.com>
2018-12-05 14:08:28 -0800

    fix it brad yang

--- notes/brad yang mad lib.py
+++ notes/brad yang mad lib.py
@@ -1,1 +1,8 @@
-mad_lib_list.append(input(" type a noun ")
+mad_lib_list = {'lulu', 'genji', 'soldier_76', 'winston', 'Reaper', 'tracer',
+        'wreaking_ball', 'Dva', 'hanzo', 'mercy', 'junk_rat'}
+
+
+mad_lib_list.append(input('type a Overwatch character')
+
+
+
