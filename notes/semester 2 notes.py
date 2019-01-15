74726b7d9bceba9528d5af69838809ae990bf109
8faces <42391893+8faces@users.noreply.github.com>
2019-01-15 14:38:55 -0800

    robot stuff

--- notes/semester 2 notes.py
+++ notes/semester 2 notes.py
@@ -1,3 +1,4 @@
+""""
 print ("hello world")
 print ("billboy")
 cars = 5
@@ -15,3 +16,12 @@ color.pop(0)
 print(color)
 print(color[1])
 print(len(color))
+"""
+
+import string
+print(list(string.ascii_letters))
+print(string.digits)
+print(string.punctuation)
+print(string.printable)
+ output = a_k_a_d
+ word = Awkward
