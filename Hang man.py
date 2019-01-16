1ea915de525865164ec5239b584b9834a91a2f9e
8faces <42391893+8faces@users.noreply.github.com>
2019-01-16 14:36:33 -0800

    stuff

--- Hang man.py
+++ Hang man.py
@@ -1,8 +1,7 @@
 import random
-guesses = 5
-playing = True
-while guesses > 0 and playing:
 word_bank = ['dog', 'catfish', 'Jose', 'frog', 'gummy',
              'cat!', 'pants', 'dragon', 'lizard', 'blank']
 word = random.choice(word_bank)
 word_bank = list(word)
+
+print("you have 8 guesses")
