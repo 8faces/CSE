6089d7265f53567ccb84191ed75f29e8cb5cebe1
8faces <42391893+8faces@users.noreply.github.com>
2019-01-22 14:37:08 -0800

    free food

--- Hang man.py
+++ Hang man.py
@@ -1,8 +1,11 @@
 import random
+import string
 word_bank = ['dog', 'catfish', 'Jose', 'frog', 'gummy',
              'cat!', 'pants', 'dragon', 'lizard', 'blank']
 word = random.choice(word_bank)
 word_bank = list(word)
-
-print("you have 8 guesses")
 guess = 8
+while guess > 0:
+    input("you have 8 guesses")
+
+for i in range(len(word_bank)):
