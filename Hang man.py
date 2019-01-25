384733f6131481c4fc571f80580afbf945b6bb1d
8faces <42391893+8faces@users.noreply.github.com>
2019-01-25 14:38:00 -0800

    hi

--- Hang man.py
+++ Hang man.py
@@ -2,14 +2,22 @@ import random
 word_bank = ['dog', 'catfish', 'Jose', 'frog', 'gummy',
              'cat!', 'pants', 'dragon', 'lizard', 'blank']
 word = random.choice(word_bank)
-word = "pants"
 word_bank = list(word)
 
-
+letter_list = []
+word = 'pants'
 guess = 8
 
+
 while guess > 0:
-    guess = input("you have 8 guesses :")
+    bush = []
+    for letter in word:
+        if letter in letter_list:
+            bush.append(letter)
+        else:
+            bush.append("_")
+    print(bush)
+    tries = input("Come and guess papa Joe's letters")
+    letter_list.append(guess)
 
-
-guess -= 1
+    guess -= 1
