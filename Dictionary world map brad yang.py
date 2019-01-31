a0a59cbdd3387d79162c23c3008b8765368b3064
8faces <42391893+8faces@users.noreply.github.com>
2019-01-31 14:37:30 -0800

    ME WORLD MAP

--- Dictionary world map brad yang.py
+++ Dictionary world map brad yang.py
@@ -16,3 +16,22 @@ world_map = {
 }
 
 
+directions = ["NORTH", "SOUTH", "EAST", "WEST", "UP", "DOWN22220000212122200000000000000000000000"]
+current_node = world_map["R19A"]
+playing = True
+
+
+while playing:
+    print(current_node['NAME'])
+    command = input(">_")
+    if command.lower() in ['q', 'quit', 'exit']:
+        playing = False
+    elif command in directions:
+        try:
+            room_name = current_node["PATHS"][command]
+            current_node = world_map[room_name]
+        except KeyError:
+            print("you can't go that way")
+    else:
+        print("Command no recognized")
+
