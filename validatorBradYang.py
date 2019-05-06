feec71dc72223188b1477d0b187d41b2d610813c
8faces <42391893+8faces@users.noreply.github.com>
2019-05-06 14:38:42 -0700

    5-6-18

--- validatorBradYang.py
+++ validatorBradYang.py
@@ -1,10 +1,4 @@
-
 test_num = "3586782824154010"
-
-
-
-
-
 
 
 def validate(num: str):
@@ -13,15 +7,25 @@ def validate(num: str):
     last_num = int(number_list.pop(15))
     print(number_list)
     print(last_num)
-    reversed_list = reverse_it(number_list)
+    number_list = reverse_it(number_list)
     print(number_list)
 
 
-def reverse_it(string):
-    string = string[::-1]
-    return string
+def reverse_it(number_list: list):
+    number_list = number_list[::-1]
+    return number_list
 
-def multiply_by_2(num: list):
+
+def multiply_by_2_subtracting_9(num: list):
+    for index in range(len(num)):
+        num[index] = int(num[index])
+        if index % 2 == 0:
+            num[index] *= 2
+            if num[index] > 9:
+                num[index] -= 9
+
+
+def mod_TEN
 
 print(validate(test_num))
 
