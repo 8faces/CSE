d231735359813e82723f39f0e2a15a000535785b
8faces <42391893+8faces@users.noreply.github.com>
2019-05-07 14:42:45 -0700

    stuff for 5 -7-19

---
+++ brad yang sales profit.py
@@ -0,0 +1,5 @@
+import csv
+
+with open("Sales Records. csv", 'r') as csv_file_stuff:
+    looker = csv.reader(csv_file_stuff)
+
--- validatorBradYang.py
+++ validatorBradYang.py
@@ -1,14 +1,16 @@
-test_num = "3586782824154010"
+import csv
 
 
 def validate(num: str):
     number_list = list(num)
     print(number_list)
-    last_num = int(number_list.pop(15))
+    last_num = number_list[15]
+    number_list.pop[15]
     print(number_list)
     print(last_num)
     number_list = reverse_it(number_list)
     print(number_list)
+    total_sum = sum(number_list)
 
 
 def reverse_it(number_list: list):
@@ -25,21 +27,20 @@ def multiply_by_2_subtracting_9(num: list):
                 num[index] -= 9
 
 
-def mod_TEN
+def mod_10(total_sum, last_num):
+    if int(total_sum) % 10 == int(last_num):
+        return True
+    return False
 
-print(validate(test_num))
 
+with open("Book1.csv", 'r') as old_csv:
+    with open("MyNewFile.csv", 'w', newline='') as new_csv:
+        print("Writing file...   ")
+        reader = csv.reader(old_csv)
+        writer = csv.writer(new_csv)
+        for row in reader:
+            old_number = row[0]  # This is a string
+            if validate(old_number):
+                writer.writerow(row)
+        print("done")
 
-# print(revers_it("olleh dlrow"))
-#
-# with open("Book1.csv", 'r') as old_csv:
-#     with open("MyNewFile.csv", 'w', newline='') as new_csv:
-#         print("Writing file...   ")
-#         reader = csv.reader(old_csv)
-#         writer = csv.writer(new_csv)
-#         for row in reader:
-#             old_number = row[0]  # This is a string
-#             if validate(old_number):
-#                 writer.writerow(row)
-#         print("done")
-#
