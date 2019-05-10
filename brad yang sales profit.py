35e85ad3378f8c554f784cb1125dc429f863d368
8faces <42391893+8faces@users.noreply.github.com>
2019-05-10 14:49:08 -0700

    ua tsaug kuv niam

--- brad yang sales profit.py
+++ brad yang sales profit.py
@@ -8,11 +8,26 @@ with open("Sales Records.csv", 'r') as csv_file_stuff:
             region = row[0]
             profit = row[13]  # This is a string
             fruit = row[2]
+            clothes = row[2]
+            snack = row[2]
+            office_supplies = row[2]
             if fruit == "Fruits":
                 print(region)
                 print(fruit)
                 print(profit)
+                print()
+
+
+
+            if clothes == "Clothes":
+
+
+                if fruit == "Snacks":
+
+
+                    if fruit == "Office Supplies":
+
+
 
 
 print("done")
-
