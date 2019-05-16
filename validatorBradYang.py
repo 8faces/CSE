1f185df4c3b089b0e7a251c82bd0e395979ccdce
8faces <42391893+8faces@users.noreply.github.com>
2019-05-16 14:41:47 -0700

    5-16-19

--- validatorBradYang.py
+++ validatorBradYang.py
@@ -3,44 +3,42 @@ import csv
 
 def validate(num: str):
     number_list = list(num)
-    print(number_list)
-    last_num = number_list[15]
-    number_list.pop[15]
-    print(number_list)
-    print(last_num)
-    number_list = reverse_it(number_list)
-    print(number_list)
+    last_number = int(number_list[15])
+    number_list.pop(15)
+    number_list = reverse(number_list)
+    multiply_and_subtract(number_list)
     total_sum = sum(number_list)
+    return mod_ten(total_sum, last_number)
 
 
-def reverse_it(number_list: list):
-    number_list = number_list[::-1]
-    return number_list
+def reverse(string):
+    string = string[::-1]
+    return string
 
 
-def multiply_by_2_subtracting_9(num: list):
+def multiply_and_subtract(num: list):
     for index in range(len(num)):
-        num[index] = int(num[index])
+        num[index] = int(num[index])    # Goes and looks at the even numbers
         if index % 2 == 0:
             num[index] *= 2
             if num[index] > 9:
                 num[index] -= 9
 
 
-def mod_10(total_sum, last_num):
-    if int(total_sum) % 10 == int(last_num):
+def mod_ten(total_sum, last_number):
+    if int(total_sum) % 10 == int(last_number):
         return True
     return False
 
 
 with open("Book1.csv", 'r') as old_csv:
     with open("MyNewFile.csv", 'w', newline='') as new_csv:
-        print("Writing file...   ")
         reader = csv.reader(old_csv)
         writer = csv.writer(new_csv)
+        print("Processing....")
         for row in reader:
-            old_number = row[0]  # This is a string
+            num = row[0]     # A String object
             if validate(num):
                 writer.writerow(row)
-        print("done")
 
+# for index in range(len(num)):
